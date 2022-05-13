c894e36b2d2d786adda24dee5cac2f997e2a6017
Andria Begiashvili <92532950+AndriaBegiashvili@users.noreply.github.com>
2022-05-14 00:12:20 +0400

    Update quiz3.py

--- quiz3.py
+++ quiz3.py
@@ -20,6 +20,19 @@ with open('music_json.json', 'w') as file:
     file.write(reqDic_list)
 
 
-print(req_dic['name'])
-print(req_dic['nb_album'])
-print(req_dic['nb_fan'])
+print("არტისტის სახელი:",req_dic['name'])
+print("ალბომების რაოდენობა:",req_dic['nb_album'])
+print("ფანები:",req_dic['nb_fan'])
+
+
+import sqlite3
+connect = sqlite3.connect('music.sqlite')
+cursor = connect.cursor()
+
+cursor.execute('''create table if not exists mus
+(id int,name varchar(700), fans int);''')
+
+cursor.execute('insert into mus(id,name,fans) values(?,?,?)', (joni,req_dic['name'],req_dic['nb_fan']) )
+
+connect.commit()
+connect.close()
